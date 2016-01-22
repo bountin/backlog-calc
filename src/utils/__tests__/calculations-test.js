0b8fb6f83b14681cdd67376ba28c4b88828bf7ce
Richard Knoll <rknoll@me.com>
2016-01-22 19:31:24 +0100

    added test cases for minimalBacklogSize

--- src/utils/__tests__/calculations-test.js
+++ src/utils/__tests__/calculations-test.js
@@ -2,7 +2,7 @@ jest.dontMock('../calculations');
 jest.dontMock('moment');
 
 import moment from 'moment';
-const {successfulProject, projectDuration, successProbability} = require('../calculations');
+const {successfulProject, projectDuration, successProbability, minimalBacklogSize} = require('../calculations');
 
 describe('calculations', () => {
 	describe('successfulProject', () => {
@@ -36,5 +36,20 @@ describe('calculations', () => {
 		it('should be quite devastating', () => {
 			expect(successProbability(moment("2015-11-30"), moment("2016-03-17"), 8, 130)).toEqual(.8);
 		});
-	})
+	});
+
+	describe('minimalBacklogSize', () => {
+		it('should calculate back from invalid projects', () => {
+			expect(minimalBacklogSize(moment("2015-11-24"), moment("2016-03-10"), 9, 120)).toEqual(108);
+		});
+		it('should calculate forward from valid projects', () => {
+			expect(minimalBacklogSize(moment("2015-11-24"), moment("2016-03-10"), 9, 100)).toEqual(108);
+		});
+		it('should calculate back from invalid projects too', () => {
+			expect(minimalBacklogSize(moment("2015-12-24"), moment("2016-04-30"), 8, 130)).toEqual(115);
+		});
+		it('should calculate forward from valid projects too', () => {
+			expect(minimalBacklogSize(moment("2015-12-24"), moment("2016-04-30"), 8, 100)).toEqual(115);
+		});
+	});
 });
