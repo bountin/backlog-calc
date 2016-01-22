5fe418fa920e1489f5cf67e60a8fc119b399f43e
Jan Michael Auer <mail@jauer.org>
2016-01-22 17:06:49 +0100

    Add texts.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames';
 
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+import Styles from './styles/Footer.less';
+
 /**
  * @TODO doc
  */
@@ -10,7 +15,14 @@ class Footer extends Component {
 	static propTypes = {};
 
 	render() {
-		return null;
+		return <footer className={Styles.footer}>
+			<div className="container">
+				<Row><Col xs={12} sm={6} lg={8} smOffset={3} lgOffset={2}>
+					<p><strong>Disclaimer:</strong> We are not saving any data you insert on the website.</p>
+					<p className={Styles.printHide}>If you wish to contact us please feel free to <a href="mailto:office@borisgloger.com?subject=Backlog%20Calculator">get in touch with us</a> - We are happy to answer your questions.</p>
+				</Col></Row>
+			</div>
+		</footer>;
 	}
 
 }
