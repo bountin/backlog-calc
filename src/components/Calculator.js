d2a0f80aab33dc9f8db9cc55481aff142b539729
Jan Michael Auer <mail@jauer.org>
2016-01-22 16:10:22 +0100

    Beautify the form just a bit.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,6 +4,8 @@ import DatePicker from 'react-datepicker';
 import classNames from 'classnames';
 import moment from 'moment';
 
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
 import Icon from 'react-fontawesome';
 import Input from 'react-bootstrap/lib/Input';
 import Button from 'react-bootstrap/lib/Button';
@@ -31,9 +33,19 @@ const messages = defineMessages({
 		defaultMessage : 'Velocity',
 	},
 
+	velocityPlaceholder : {
+		id             : 'calculator.velocityPlaceholder',
+		defaultMessage : 'Story Points per week',
+	},
+
 	backlogSizeLabel : {
 		id             : 'calculator.backlogSizeLabel',
 		defaultMessage : 'Backlog Size',
+	},
+
+	backlogSizePlaceholder : {
+		id             : 'calculator.backlogSizePlaceholder',
+		defaultMessage : 'Total Story Points',
 	},
 
 	submitLabel : {
@@ -71,22 +83,41 @@ class Calculator extends Component {
 		}
 
 		return <section className="container">
+			<Row>
+				<Col xs={12} sm={6} lg={8} smOffset={3} lgOffset={2}>
+					<p>
+						Dear ProductOwner,
+					</p>
+					<p>
+						On this page you have the possibility to calculate, if you will be able to keep your commitment
+						towards your stakeholders. If you have an estimated backlog, the velocity of the team per week,
+						the start date of the project and the choosen date of delivery, you have everything that is
+						necessary in order to find out if you can deliver within the given timeframe.
+					</p>
+					<p>
+						We wish you a lot of success with your Product development, <br />
+						Your borisgloger-Team
+					</p>
+					<p>&nbsp;</p>
+				</Col>
+			</Row>
 			<form className="form-horizontal" onSubmit={::this.recalculate}>
 				<Input
 					type="text"
 					ref="projectName"
 					label={intl.formatMessage(messages.projectNameLabel)}
 					labelClassName="col-xs-12 col-sm-3 col-lg-2"
-					wrapperClassName="col-xs-12 col-sm-9 col-lg-10"
+					wrapperClassName="col-xs-12 col-sm-6 col-lg-8"
 					onChange={::this.reset}
 				/>
 
 				<Input
 					label={intl.formatMessage(messages.startDateLabel)}
 					labelClassName="col-xs-12 col-sm-3 col-lg-2"
-					wrapperClassName="col-xs-12 col-sm-9 col-lg-10">
+					wrapperClassName="col-xs-12 col-sm-6 col-lg-8">
 					<DatePicker
 						ref="startDate"
+						className="form-control"
 						locale={intl.locale}
 						selected={startDate || moment()}
 						maxDate={endDate}
@@ -97,10 +128,11 @@ class Calculator extends Component {
 				<Input
 					label={intl.formatMessage(messages.endDateLabel)}
 					labelClassName="col-xs-12 col-sm-3 col-lg-2"
-					wrapperClassName="col-xs-12 col-sm-9 col-lg-10">
+					wrapperClassName="col-xs-12 col-sm-6 col-lg-8">
 					<DatePicker
 						ref="endDate"
 						locale={intl.locale}
+						className="form-control"
 						selected={endDate || moment().add(1, 'month')}
 						minDate={startDate}
 						onChange={::this.reset}
@@ -111,8 +143,9 @@ class Calculator extends Component {
 					type="number"
 					ref="velocity"
 					label={intl.formatMessage(messages.velocityLabel)}
+					placeholder={intl.formatMessage(messages.velocityPlaceholder)}
 					labelClassName="col-xs-12 col-sm-3 col-lg-2"
-					wrapperClassName="col-xs-12 col-sm-9 col-lg-10"
+					wrapperClassName="col-xs-12 col-sm-6 col-lg-8"
 					onChange={::this.reset}
 					min="0"
 				/>
@@ -121,8 +154,9 @@ class Calculator extends Component {
 					type="number"
 					ref="backlogSize"
 					label={intl.formatMessage(messages.backlogSizeLabel)}
+					placeholder={intl.formatMessage(messages.backlogSizePlaceholder)}
 					labelClassName="col-xs-12 col-sm-3 col-lg-2"
-					wrapperClassName="col-xs-12 col-sm-9 col-lg-10"
+					wrapperClassName="col-xs-12 col-sm-6 col-lg-8"
 					onChange={::this.reset}
 					min="0"
 				/>
@@ -160,7 +194,7 @@ class Calculator extends Component {
 		const backlogSize = this.refs.backlogSize.getValue();
 
 		this.setState({
-			result: true,
+			result : true,
 		});
 	}
 
