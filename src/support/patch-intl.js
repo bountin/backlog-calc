8dd6ac2eaf949d324166be1e0413dba6d99c297d
Richard Knoll <rknoll@me.com>
2016-01-26 11:02:23 +0100

    fixed internationalization for safari

--- src/support/patch-intl.js
+++ src/support/patch-intl.js
@@ -3,8 +3,10 @@
 // see http://formatjs.io/guides/runtime-environments/#client
 export default function patchIntl(run) {
 	if (global.Intl) return run();
-	require.ensure(['intl'], require => {
+	require.ensure([], require => {
 		require('intl');
+		require('intl/locale-data/jsonp/en.js');
+		require('intl/locale-data/jsonp/de.js');
 		run();
 	}, 'IntlBundle');
 }
