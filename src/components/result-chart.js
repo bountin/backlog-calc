41aeb7f5b726b51c2a2f32d48283755e78ac804d
Jan Michael Auer <ja@25th-floor.com>
2016-03-04 10:58:20 +0100

    Fix remaining linter errors in Chart.

--- src/components/result-chart.js
+++ src/components/result-chart.js
@@ -1,15 +1,8 @@
 import d3 from 'd3';
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames';
 
 import Chart from './svg/chart';
-import ClipRect from './svg/clip-rect';
-import Axis from './svg/axis';
-import Area from './svg/area';
-import Line from './svg/line';
-import Marker from './svg/marker';
-import Indicator from './svg/indicator';
-
 import Styles from './styles/chart.less';
 
 /**
@@ -42,7 +35,7 @@ class ResultChart extends Component {
          * frame. Number greater or equal to zero.
          */
         backlogSize: PropTypes.number.isRequired,
-        
+
         /**
          * Start Date of the Project
          */
@@ -54,13 +47,6 @@ class ResultChart extends Component {
         endDate: PropTypes.object.isRequired,
     };
 
-    static defaultProps = {
-        today: new Date(),
-    };
-
-    monthFormat = d3.time.format('%b');
-    yearFormat = d3.time.format('%Y');
-
     constructor(props) {
         super(props);
         this._resize = ::this._resize;
@@ -70,62 +56,20 @@ class ResultChart extends Component {
         };
     }
 
-    componentWillReceiveProps(props) {
-        const { balance } = props;
-        if (balance && balance.length) {
-            this.setState({ scales: this._computeScales(balance, this.state.size) });
-        }
-    }
-
     componentDidMount() {
         this._resize();
         window.addEventListener('resize', this._resize);
     }
 
+    componentWillReceiveProps(props) {
+        const { balance } = props;
+        if (balance && balance.length) {
+            this.setState({ scales: this.computeScales(props, this.state.size) });
+        }
+    }
+
     componentWillUnmount() {
         window.removeEventListener('resize', this._resize);
-    }
-
-    getTickFormat(year, d) {
-        return (d.getFullYear() !== year) ? (year === d.getFullYear()) && this.yearFormat(d) : this.monthFormat(d);
-    }
-
-    render() {
-        const {
-            isSuccessful,
-            completionDate,
-            probability,
-            backlogSize
-        } = this.props;
-
-        const {
-            scales,
-            size,
-            padding,
-            hovered,
-        } = this.state;
-
-        if (!size || !scales) {
-            return <div ref={ c => { this.node = c; }} />;
-        }
-
-        const offset = this._calculateOffset();
-        let year = 0;
-
-        let tickFormat = this.getTickFormat.bind(this, year);
-
-        return <div ref={ c => { this.node = c; }}>
-            <Chart
-                width={size.width}
-                height={size.height}
-                padding={padding}
-                onClick={::this._onClick}
-                onMouseMove={::this._onMouseMove}
-                style={{ top: offset }}
-                className={classNames(Styles.chart)}
-            >
-            </Chart>
-        </div>;
     }
 
     /**
@@ -139,7 +83,7 @@ class ResultChart extends Component {
      * @return {number[]} An array containing the domain boundaries.
      * @private
      */
-    _computeDomain(data) {
+    computeDomain(data) {
         const value = d => d.pct50;
 
         const min = d3.min(data, value);
@@ -159,19 +103,18 @@ class ResultChart extends Component {
      * on the SVG graph. The scales object contains two components, 'x'
      * and 'y'.
      *
-     * @param {Array} balance The full balance history.
      * @param {{width: number, height: number}} size The size of the chart.
      * @return {{x: function, y: function}} The new scales object.
      * @private
      */
-    _computeScales(balance, size) {
+    computeScales({ startDate, endDate }, size) {
         return {
             x: d3.time.scale()
                 .range([0, size.width])
-                .domain(d3.extent(balance, d => d.date)),
+                .domain([startDate, endDate]),
             y: d3.scale.linear()
                 .range([size.height, 0])
-                .domain(this._computeDomain(balance))
+                .domain(d3.scale.ordinal().domain('Project')),
         };
     }
 
@@ -179,7 +122,7 @@ class ResultChart extends Component {
      * @TODO doc
      * @private
      */
-    _resize() {
+    resize() {
         const container = this.node;
         const { padding } = this.state;
         const size = {
@@ -189,84 +132,29 @@ class ResultChart extends Component {
 
         this.setState({
             size,
-            scales: this._computeScales(this.props.balance, size),
+            scales: this.computeScales(this.props, size),
         });
     }
 
-    /**
-     * Updates the indicator to match the current mouse position.
-     *
-     * @param {MouseEvent} event The mouse event payload.
-     * @private
-     */
-    _onMouseMove(event) {
-        const { balance } = this.props;
+    render() {
+        const {
+            scales,
+            size,
+            padding,
+        } = this.state;
 
-        const date = this._getEventDate(event);
-        const hovered = first(balance, d => Math.abs(d.date - date) < 12 * 3600 * 1000);
-
-        // TODO: Performance -> Is rerendering the complete component too slow?
-        this.setState({ hovered });
-    }
-
-    /**
-     * Creates a new event at the position of the mouse click.
-     *
-     * @param {MouseEvent} event The mouse event payload.
-     * @private
-     */
-    _onClick(event) {
-        const date = this._getEventDate(event);
-        EventActions.create(date);
-    }
-
-    /**
-     * Computes the date at the position of the given mouse event. The
-     * position is converted using 'scales.x' in the current scope.
-     *
-     * @param {MouseEvent} event The mouse event payload.
-     * @returns {Date} The date at the current mouse position.
-     * @private
-     */
-    _getEventDate(event) {
-        const { scales, padding } = this.state;
-
-        d3.event = event;
-        const [x] = d3.mouse(event.currentTarget);
-        return scales.x.invert(x - padding.left);
-    }
-
-    /**
-     * @TODO doc
-     *
-     * @returns {object} The balance object containing date and amount.
-     * @private
-     */
-    _calculateOffset() {
-        const { balance, selectedEvent } = this.props;
-        const { scales, size } = this.state;
-
-        if (selectedEvent == null) {
-            return 0;
+        if (!size || !scales) {
+            return <div ref={ c => { this.node = c; }} />;
         }
 
-        const b = first(balance, d => Math.abs(d.date - selectedEvent.date) < 12 * 3600 * 1000);
-        return -scales.y(b.pct50) + size.height * .15;
+        return <div ref={ c => { this.node = c; }}>
+            <Chart
+                width={size.width}
+                height={size.height}
+                padding={padding}
+                className={classNames(Styles.chart)}
+            />
+        </div>;
     }
 
-}
-
-/**
- * Iterates the given collection and returns the first element matching
- * the given filter function. The filter function must return truthy for
- * any matching element or falsy otherwise.
- *
- * TODO: Move to utils
- *
- * @param {Array}    collection The collection to iterate.
- * @param {Function} filter     A function to find matching elements.
- * @returns {*} The first matching element or null, if none.
- */
-function first(collection, filter) {
-    return collection.filter(filter)[0];
 }
